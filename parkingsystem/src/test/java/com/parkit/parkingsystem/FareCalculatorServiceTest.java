9d43e6b426b2dde561c4591246df69efa81b67a0
Joel Dumortier <f8mkr7w53272-student@anonymous-codio.com>
2023-02-23 12:58:02 +0000

    add test for a car with a ticket of less than 30 minutes

--- parkingsystem/src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -123,4 +123,20 @@ public class FareCalculatorServiceTest {
         assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
     }
 
+    @Test
+    public void calculateFareCarWithLessThan30minutesParkingTime(){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - ( 15 * 60 * 1000))
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1,ParkingSpot.CAR, false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals(0, ticket.getPrice());
+    }
+
+    
+
 }
