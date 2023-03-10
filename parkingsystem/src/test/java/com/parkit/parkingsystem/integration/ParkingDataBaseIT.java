ae35727074e518b464f96693e3a0754dea8cc263
Joel Dumortier <f8mkr7w53272-student@anonymous-codio.com>
2023-03-10 14:21:57 +0000

    add new test testParkingLotExitRecurringUser

--- parkingsystem/src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,11 +1,13 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
+import com.parkit.parkingsystem.model.Ticket;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +19,11 @@ import static org.mockito.Mockito.when;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import java.util.Date;
+import java.lang.Thread;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -62,15 +69,34 @@ public class ParkingDataBaseIT {
         assertFalse(ticketDAO.getTicket("ABCDEF").getParkingSpot().isAvailable());
     }
 
-    
     @Test
     public void testParkingLotExit() throws Exception {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
+        Thread.sleep(1000);
         parkingService.processExitingVehicle();
+        Thread.sleep(1000);
         //TODO: check that the fare generated and out time are populated correctly in the database
         assertEquals(0, ticketDAO.getTicket("ABCDEF").getPrice());
         assertNotNull(ticketDAO.getTicket("ABCDEF").getOutTime());
     }
 
+    @Test
+    public void testParkingLotExitRecurringUser() throws Exception {
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
+        Ticket ticket1 = ticketDAO.getTicket("ABCDEF");
+        ticket1.setInTime(new Date(ticketDAO.getTicket("ABCDEF").getInTime().getTime() - (60*60*1000)));
+        ticketDAO.updateTicketIntime(ticket1);
+        parkingService.processExitingVehicle();
+        Thread.sleep(1000);
+        parkingService.processIncomingVehicle();
+        Ticket ticket2 = ticketDAO.getTicket("ABCDEF");
+        ticket2.setInTime(new Date(ticketDAO.getTicket("ABCDEF").getInTime().getTime() - (30*60*1000)));
+        ticketDAO.updateTicketIntime(ticket2);
+        parkingService.processExitingVehicle();
+        BigDecimal expectedPrice = BigDecimal.valueOf((30/(double)60) * Fare.CAR_RATE_PER_HOUR * 0.95).setScale(2, RoundingMode.CEILING);
+        BigDecimal ticketPrice = BigDecimal.valueOf(ticketDAO.getTicket("ABCDEF").getPrice()).setScale(2, RoundingMode.CEILING);
+        assertEquals(expectedPrice,ticketPrice); 
+    }
 }
