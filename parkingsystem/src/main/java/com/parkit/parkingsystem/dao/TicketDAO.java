b5bae72b19c46ee3ec9d42c712f00999ff230121
Joel Dumortier <f8mkr7w53272-student@anonymous-codio.com>
2023-03-10 14:18:07 +0000

    create new method to update the inTime of a ticket in the database

--- parkingsystem/src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ parkingsystem/src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -87,6 +87,23 @@ public class TicketDAO {
         return false;
     }
 
+    public boolean updateTicketIntime(Ticket ticket) {
+        Connection con = null;
+        try {
+            con = dataBaseConfig.getConnection();
+            PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_TICKET_INTIME);
+            ps.setTimestamp(1, new Timestamp(ticket.getInTime().getTime()));
+            ps.setInt(2,ticket.getId());
+            ps.execute();
+            return true;
+        }catch (Exception ex){
+            logger.error("Error saving ticket info",ex);
+        }finally {
+            dataBaseConfig.closeConnection(con);
+        }
+        return false;
+    }
+
     public int getNbTicket(String vehicleRegNumber){
         Connection con = null;
         int nbTicket = 0;
